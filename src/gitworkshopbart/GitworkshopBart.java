03c8a6007263d8421343e60e254834867f0ac433
Bart <dekort.bart@gmail.com>
2020-04-16 11:22:50 +0200

    merge login to master

--- src/gitworkshopbart/GitworkshopBart.java
+++ src/gitworkshopbart/GitworkshopBart.java
@@ -3,7 +3,7 @@ package gitworkshopbart;
 public class GitworkshopBart {
 
     public static void main(String[] args) {
-        System.out.println("Hello Login");
+        System.out.println("Hello Bart");
         LoginController login = new LoginController();
     }
     
