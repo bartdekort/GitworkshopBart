707571b9bc15c65acdfedbc91495d7b28d0a642a
Bart <dekort.bart@gmail.com>
2020-04-16 10:48:36 +0200

    login controller

--- src/gitworkshopbart/GitworkshopBart.java
+++ src/gitworkshopbart/GitworkshopBart.java
@@ -3,7 +3,8 @@ package gitworkshopbart;
 public class GitworkshopBart {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Login");
+        LoginController login = new LoginController();
     }
     
 }
